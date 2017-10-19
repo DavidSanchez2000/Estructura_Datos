ddb4229eb08178196eaf46946ed69a33d9653390
dsanchez <davidsanchez2000@javerianacali.edu.co>
2017-10-19 11:10:29 -0500

    version

--- Lista.cpp
+++ Lista.cpp
@@ -4,8 +4,46 @@ using namespace std;
 
 template <class T> Lista<T> :: Lista():vector<T>() {};
 
-template <class T> void Lista<T>::adicionarElemento(T element ,int Pos)
+template <class T> void Lista<T>::insertar(T element ,int pos)
 {
-	this -> insert(this ->begin()+ Pos,element);
+	this -> insert(this ->begin()+ pos,element);
 }
 
+template <class T> T Lista<T>::eliminar(int pos)
+{
+	T element =  this->at(pos);
+	this->erase(this->begin() + pos);
+	return element;
+}
+
+template <class T> void Lista<T>::mostrar()
+{
+	for(int i = 0; i< this->size();i++)
+	{
+		cout<<"Elemento"<< this->at(i)<<endl;
+	}
+}
+
+template <class T> int Lista<T>::longitud()
+{
+	int L = this->size();
+	cout<<"Longitud"<< this->size();
+}
+
+template <class T> void Lista<T>::adicionarElemento(T element)
+{
+	this->push_back(element);
+}
+
+template <class T> void Lista <T>::buscar(T element )
+
+{
+	vector <T> tmpl(this);
+	int n = this->size();
+	for (int i = 0; i < n; i++)
+	{
+		if(this->push_back(i)== element)
+		{	return element;
+		}
+	}
+}
--- Lista.h
+++ Lista.h
@@ -11,15 +11,13 @@ template <class T> class  Lista:public vector <T>
 	public:
 		Lista();
 //		~Lista();
-	    void adicionarElemento(T element,int Pos);
+                void adicionarElemento(T element);
 		T eliminar(int pos);
 		void mostrar();
 		int longitud();
-		void insertar(T);
+		void insertar(T element, int pos);
 		void buscar(T);
 		void getelemento(int pos);
 		void setElemento(T,int pos);
-		
-				
 };
 #endif
